dd2f968f4f645992ff3f33d9af7bd85f3800eab5
nils fedrigo <nils.fedrigo@student-cs.fr>
2018-11-21 15:50:12 +0100

    total rework of 2048

--- game2048/move.py
+++ game2048/move.py
@@ -1,6 +1,126 @@
 import numpy as np
 import copy as copy
-from game2048.grid import *
+import random as rd
+import copy
+
+THEMES = {"0": {"name": "Default", 0: "", 2: "2", 4: "4", 8: "8", 16: "16", 32: "32", 64: "64", 128: "128", 256: "256",
+                512: "512", 1024: "1024", 2048: "2048", 4096: "4096", 8192: "8192"},
+          "1": {"name": "Chemistry", 0: "", 2: "H", 4: "He", 8: "Li", 16: "Be", 32: "B", 64: "C", 128: "N", 256: "O",
+                512: "F", 1024: "Ne", 2048: "Na", 4096: "Mg", 8192: "Al"},
+          "2": {"name": "Alphabet", 0: "", 2: "A", 4: "B", 8: "C", 16: "D", 32: "E", 64: "F", 128: "G", 256: "H",
+                512: "I", 1024: "J", 2048: "K", 4096: "L", 8192: "M"}}
+
+
+
+#-------------------GRID -----------
+
+def create_grid(n):
+    grid_inst = []
+    grid_game = [[' ' for i in range(n)]for j in range(n)]
+    return grid_game
+
+
+def grid_add_new_tile_at_position(game_grid, x, y):
+    choice = rd.random()
+    if choice <= 0.9:
+        tile = 2
+    else:
+        tile = 4
+    game_grid[x][y] = tile
+
+    return game_grid
+
+
+def get_all_tiles(game_grid, arg=0):
+    get_tiles = []
+    for i in range(len(game_grid)):
+        for j in range(len(game_grid)):
+            if game_grid[i][j] == ' ' and arg == 0:
+                get_tiles += [0]
+            else:
+
+                get_tiles += [game_grid[i][j]]
+
+    return get_tiles
+
+
+def get_empty_tiles_positions(grid):
+    empty_position = []
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 0 or grid[i][j] == ' ':
+                empty_position += [(i, j)]
+    return empty_position
+
+
+def get_new_position(grid):
+    empty = get_empty_tiles_positions(grid)
+    return rd.choice(empty)
+
+
+def grid_get_value(grid, x, y):
+    if grid[x][y] == ' ':
+        return 0
+    return grid[x][y]
+
+
+def grid_add_new_tile(grid):
+    position = get_new_position(grid)
+    return grid_add_new_tile_at_position(grid, position[0], position[1])
+
+
+def init_game(n):
+    grid = create_grid(n)
+    #print("created grid", grid)
+    grid = grid_add_new_tile(grid)
+    #print("first tile", grid)
+    grid = grid_add_new_tile(grid)
+    #print("second tile", grid)
+    return grid
+
+
+# visual interface
+
+def length(grid, theme='0'):
+    tiling = get_all_tiles(grid, 0)
+    return max(
+        len(THEMES[theme][tiling[i]])
+        for i in range(2))
+
+
+def grid_to_string(grid, n, theme='0'):
+    print(grid)
+    grid2 = copy.deepcopy(grid)
+    tiles = get_all_tiles(grid2, 0)
+    #print('tiles',tiles)
+    printed_grid = """"""
+    length_here = length(grid2, theme)
+    for i in range(n):
+
+        beginning = '=' * (length_here + 2)
+        top = ' ' + beginning + ' ' + beginning + ' ' + beginning + ' ' + beginning + ' '
+        middle = ''
+        for j in range(n):
+            middle += '|' + (THEMES[theme][tiles[(n) * i + j]]).center(length_here + 2)
+
+        middle += '|'
+        printed_grid += top + '\n' + middle + '\n'
+    printed_grid += top
+    #print(printed_grid)
+    return printed_grid
+
+#grid_to_string([[16, 4, 8, 2], [2, 4, 2, 128], [4, 512, 32, 64], [1024, 2048, 512, 2]], 4, theme='2')
+#print(create_grid(2))
+#print(grid_add_new_tile_at_position([[' ', ' '], [' ', 4]],1,0))
+#print(init_game(4))
+
+
+
+
+
+
+
+#-------------------MOVES -----------
 
 
 def move_left(grid):
@@ -24,7 +144,6 @@ def move_left(grid):
                         lines[k+2] = ' '
                         lines[k+1] = lines[k+1]*2
                         flag[counter - 1][k+1] = 1
-
     return grid
 
 def move_right(grid):
@@ -64,8 +183,9 @@ def move_top(grid):
     for i in range(len(grid)):
         for j in range(len(grid)):
             grid_final[i][j] = grid_temp[j][i]
-
     return grid_final
+
+
 
 
 def move_grid(grid, d):
@@ -78,7 +198,31 @@ def move_grid(grid, d):
         grid2 = move_top(grid)
     if d == 'b':
         grid2 = move_bottom(grid)
+
     return grid2
+
+
+
+
+def complete_move_top(grid):
+    grid = move_top(grid)
+    grid = grid_add_new_tile(grid)
+    return grid
+
+def complete_move_right(grid):
+    grid = move_right(grid)
+    grid = grid_add_new_tile(grid)
+    return grid
+def complete_move_left(grid):
+    grid = move_left(grid)
+    grid = grid_add_new_tile(grid)
+    return grid
+
+def complete_move_bottom(grid):
+    grid = move_bottom(grid)
+    grid = grid_add_new_tile(grid)
+    return grid
+
 
 
 def is_grid_full(grid):
@@ -89,7 +233,7 @@ def is_grid_full(grid):
                 is_it = False
     return is_it
 
-"""def move_possible(grid):
+"""def moves _possible(grid):                                   #
     grid_ok = copy.deepcopy(grid)
     grid_for_test = copy.deepcopy(grid)
     is_possible = {'g':False,'d':False,'h':False,'b':False}
@@ -105,7 +249,7 @@ def is_grid_full(grid):
 
 def is_game_over(grid):
     possible_move = []
-    for key , value in move_possible(grid).items():
+    for key , value in moves_possible(grid).items():
         possible_move+=[value]
     return is_grid_full(grid) and not(True in possible_move)
 
@@ -116,7 +260,7 @@ def is_game_over(grid):
 
 
 
-def move_possible(grid):
+def moves_possible(grid):
     grid_changed = copy.deepcopy(grid)
     grid_reference = copy. deepcopy(grid)
 
@@ -134,4 +278,29 @@ def move_possible(grid):
     grid_changed = copy.deepcopy(grid)
 
     return is_possible
-print(move_possible([[32, 8, 4, ' '], [2, 8, 4, ' '], [16, 2, 8, ' '], [16, 4, 2, 4]]))
+
+def move_possible(grid, text_move_fonction):
+    dict_possible = moves_possible(grid)
+    if text_move_fonction == 'complete_move_top':
+        print(dict_possible['h'])
+        return dict_possible['h']
+    if text_move_fonction == 'complete_move_bottom':
+        return dict_possible['b']
+    if text_move_fonction == 'complete_move_left':
+        return dict_possible['g']
+    if text_move_fonction == 'complete_move_right':
+        return dict_possible['d']
+
+
+
+
+#-------------------INFO NECESSARY-----------
+GRID_LEN=4
+color_dico= {' ':["#9e948a",''],2:["#eee4da",'2'], 4:["#ede0c8",'4'], 8:["#f2b179",'8'],16:["#f59563",'16'], \
+                            32:["#f67c5f",'32'], 64:["#f65e3b",'64'], 128:["#edcf72",'128'], 256:["#edcc61",'256'], \
+                            512:["#edc850","512"], 1024:["#edc53f",'1024'], 2048:["#edc22e",'2048'] }
+dico_command={1:[mv.complete_move_top,"Up",[0,1]],2:[mv.complete_move_bottom,"Down",[2,1]],3:[mv.complete_move_right,"Right",[1,2]],4:[mv.complete_move_left,"Left",[1,0]]}
+GRID_PADDLE=5
+SIZE=500
+def info_necessary():
+    return GRID_LEN, GRID_PADDLE, dico_command, color_dico,init_game, is_game_over, move_possible, is_game_won
--- launch_a_game.py
+++ launch_a_game.py
@@ -4,13 +4,8 @@ from interface_graphique.interface import *
 import game2048.move as mv
 import time
 
-GRID_LEN=4
-color_dico= {' ':["#9e948a",''],2:["#eee4da",'2'], 4:["#ede0c8",'4'], 8:["#f2b179",'8'],16:["#f59563",'16'], \
-                            32:["#f67c5f",'32'], 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", \
-                            512:"#edc850", 1024:"#edc53f", 2048:"#edc22e" }
-dico_command={1:[mv.move_top,"Up",[0,1]],2:[mv.move_bottom,"Down",[2,1]],3:[mv.move_right,"Right",[1,2]],4:[mv.move_left,"Left",[1,0]]}
-GRID_PADDLE=5
-SIZE=500
+
+GRID_LEN, GRID_PADDLE, dico_command, color_dico,init_game, is_game_over, move_possible, is_game_won = mv.launch_a_game()
 
 def launch_a_game():
     windows_game=Tk()
@@ -21,6 +16,5 @@ def launch_a_game():
     windows_game.mainloop()
     windows_command.mainloop()
 
-launch_a_game()
 
-print(mv.move_right([[2, 2, ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ']]))
+launch_a_game()
